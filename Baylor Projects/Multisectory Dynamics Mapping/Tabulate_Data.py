8013e7c57e7a407766ec66b792f5d06fa09c718b
Jayjayanton <jao517@alumni.york.ac.uk>
2023-11-01 16:17:58 -0500

    Added dbf to excel segment to code

--- Baylor Projects/Multisectory Dynamics Mapping/Tabulate_Data.py
+++ Baylor Projects/Multisectory Dynamics Mapping/Tabulate_Data.py
@@ -7,6 +7,10 @@ import arcpy
 from arcpy.sa import *
 from arcpy import env
 
+#######################################################################################################################################
+### Predominantly uses the Arcpy tool 'Tabulate Area' to find the area of the MSD basemap for the year 2008 overlayed with all PFTs ###
+#######################################################################################################################################
+
 Tiff_files = sorted(glob.glob("*.tif"))
 
 arcpy.env.workspace = r"D:\\GCAM\\Landcover\\CLM\\PFT_78\\Tabulated Data\\"
@@ -19,10 +23,28 @@ zone_field = "Value"
 class_field = "Value"
 processingCellSize = 30
 
-
 for tiff in Tiff_files:
     basetfilename = os.path.basename(tiff)
     CleanBaseStr = os.path.splitext(basetfilename)[0]
     OutFileName = f"{CleanBaseStr}.dbf"
     arcpy.sa.TabulateArea(tiff, zone_field, inlayer, class_field, OutFileName, processingCellSize, "CLASSES_AS_FIELDS")
     print(f"Done:{CleanBaseStr}")
+
+### Converts dbf files into usable excel files ###
+
+os.chdir(arcpy.env.workspace)
+
+Dbf_files = sorted(glob.glob("*.dbf"))
+
+arcpy.env.workspace = r"D:\\GCAM\\Landcover\\CLM\\PFT_78\\Tabulated Data\\"
+arcpy.env.overwriteOutput = True
+
+Excel_Folder = r"D:\\GCAM\\Landcover\\CLM\\PFT_78\\Excel Files\\Tabulate Area V_4_19"
+
+for Name in Dbf_files:
+    basetfilename = os.path.basename(Name)
+    CleanBaseStr = os.path.splitext(basetfilename)[0]
+    PFT = CleanBaseStr.split('_',-1)[8]
+    out_xls = f"PFT_{PFT}.xlsx"
+    output_file = os.path.join(Excel_Folder, out_xls)
+    arcpy.conversion.TableToExcel(Name, output_file)
